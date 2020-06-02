e783c5be82c724adb2d8677f7c3cc22ee7cad762
Romain Laurent <romain.laurent@cdiscount.com>
2020-06-02 10:13:38 +0200

    feat: search input and filter recipes

--- src/addSearchInput.js
+++ src/addSearchInput.js
@@ -1,6 +1,7 @@
 export default function addSearchInput(element) {
   const searchInput = document.createElement("input");
   searchInput.setAttribute("type", "search");
+  searchInput.setAttribute("id", "ingredients");
   searchInput.setAttribute("name", "ingredients");
   searchInput.setAttribute("placeholder", "Ingrédients…");
   searchInput.classList.add("search-input");
--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+import { all, includes } from "ramda";
+
 import addSearchInput from "./addSearchInput";
 import addRecipe from "./addRecipe";
 import responseToJson from "./responseToJson";
@@ -9,11 +11,40 @@ export default function app() {
 
   addSearchInput(header);
 
+  let recipes = [];
+  let filteredIngedients = [];
+
+  const displayRecipes = () => {
+    while (recipesList.hasChildNodes()) {
+      recipesList.removeChild(recipesList.firstChild);
+    }
+
+    const filteredRecipes = recipes.filter((recipe) => {
+      const includesIngredients = (val) => includes(val, recipe.ingredients);
+
+      return all(includesIngredients, filteredIngedients);
+    });
+
+    filteredRecipes.map((recipe) => {
+      addRecipe(recipesList, recipe);
+    });
+  };
+
+  const filterRecipes = (event) => {
+    const searchInput = event.currentTarget;
+    filteredIngedients = searchInput.value.split(" ");
+
+    displayRecipes();
+  };
+
+  const searchInput = document.getElementById("ingredients");
+  searchInput.addEventListener("keyup", filterRecipes);
+  searchInput.addEventListener("change", filterRecipes);
+
   fetch("http://localhost:3000/recipes")
     .then(responseToJson)
-    .then((recipes) => {
-      recipes.map((recipe) => {
-        addRecipe(recipesList, recipe);
-      });
-    });
+    .then((fetchedRecipes) => {
+      recipes = fetchedRecipes;
+    })
+    .then(displayRecipes);
 }
